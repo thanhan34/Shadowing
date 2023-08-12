a355e073ab5616e87c5956d72d54d2c4c361bca6
An Doan <dtan42@gmail.com>
2023-08-13 04:02:49 +1000

    paragraph

--- shadowing/pages/paragraph.tsx
+++ shadowing/pages/paragraph.tsx
@@ -43,45 +43,45 @@
 // // };
 
 // // export default Paragraph;
-// import React, { useEffect, useState } from "react";
-// import { getDownloadURL, ref, listAll } from "firebase/storage";
-// import { storage } from "../firebase"; // Make sure to import the storage instance correctly
+import React, { useEffect, useState } from "react";
+import { getDownloadURL, ref, listAll } from "firebase/storage";
+import { storage } from "../firebase"; // Make sure to import the storage instance correctly
 
-// const Paragraph = () => {
-//   const [downloadLinks, setDownloadLinks] = useState<{ name: string; url: string }[]>([]);
+const Paragraph = () => {
+  const [downloadLinks, setDownloadLinks] = useState<{ name: string; url: string }[]>([]);
 
-//   useEffect(() => {
-//     const fetchDownloadLinks = async () => {
-//       try {
-//         const storageRef = ref(storage, "gs://pteshadowing.appspot.com"); // Update with your storage folder
-//         const files = await listAll(storageRef);
+  useEffect(() => {
+    const fetchDownloadLinks = async () => {
+      try {
+        const storageRef = ref(storage, "gs://pteshadowing.appspot.com"); // Update with your storage folder
+        const files = await listAll(storageRef);
 
-//         const downloadPromises = files.items.map(async (file) => {
-//           const downloadURL = await getDownloadURL(file);
-//           return { name: file.name, url: downloadURL };
-//         });
+        const downloadPromises = files.items.map(async (file) => {
+          const downloadURL = await getDownloadURL(file);
+          return { name: file.name, url: downloadURL };
+        });
 
-//         const links = await Promise.all(downloadPromises);
-//         setDownloadLinks(links);
-//       } catch (error) {
-//         console.error("Error fetching download links:", error);
-//       }
-//     };
+        const links = await Promise.all(downloadPromises);
+        setDownloadLinks(links);
+      } catch (error) {
+        console.error("Error fetching download links:", error);
+      }
+    };
 
-//     fetchDownloadLinks();
-//   }, []);
+    fetchDownloadLinks();
+  }, []);
 
-//   return (
-//     <main className="flex flex-col items-center justify-between min-h-screen p-24">
-//       <h1>Download Links</h1>
-//       <ul>
-//         {downloadLinks.map((link) => (
-//           <p>{link.name},{link.url}</p>
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <h1>Download Links</h1>
+      <ul>
+        {downloadLinks.map((link) => (
+          <p>{link.name},{link.url}</p>
           
-//         ))}
-//       </ul>
-//     </main>
-//   );
-// };
+        ))}
+      </ul>
+    </main>
+  );
+};
 
-// export default Paragraph;
+export default Paragraph;
