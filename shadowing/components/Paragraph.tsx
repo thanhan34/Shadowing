9587fcfdac5447dac47765eb1bb9c890953fe0cf
An Doan <dtan42@gmail.com>
2023-08-13 11:51:08 +1000

    new way to load html string

--- shadowing/components/Paragraph.tsx
+++ shadowing/components/Paragraph.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import parse from 'html-react-parser'
 interface ParagraphProps {
   videoSource: string;
   sentence: string;
@@ -12,13 +12,13 @@ const Paragraph: React.FC<ParagraphProps> = ({ videoSource, sentence }) => {
         <audio controls key={videoSource}>
           <source src={videoSource} type="audio/mpeg" />
         </audio>
-        <p className="mt-5 mb-5 text-2xl" dangerouslySetInnerHTML={{ __html: sentence }}></p>
+        <p className="mt-5 mb-5 text-2xl" >{parse(sentence)}</p>
         <div className="flex flex-row pt-5 pb-5 space-x-10">
-        <p className="p-3 border border-red-500 rounded-full ra-break">Pause</p>
-        <p className="p-3 border border-orange-500 rounded-full ra-loss">Loss</p>
-        <p className="p-3 border rounded-full border-cyan-400 ra-link">Linking</p>
-        <p className="p-3 border border-gray-500 rounded-full ra-weak ">Weak</p>
-      </div>
+          <p className="p-3 border border-red-500 rounded-full ra-break">Pause</p>
+          <p className="p-3 border border-orange-500 rounded-full ra-loss">Loss</p>
+          <p className="p-3 border rounded-full border-cyan-400 ra-link">Linking</p>
+          <p className="p-3 border border-gray-500 rounded-full ra-weak ">Weak</p>
+        </div>
       </div>
       
     </div>
--- shadowing/components/Sentence.tsx
+++ shadowing/components/Sentence.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import parse from 'html-react-parser'
 interface SentenceProps {
   videoSource: string;
   sentence: string;
@@ -12,7 +12,7 @@ const Sentence: React.FC<SentenceProps> = ({ videoSource, sentence }) => {
         <audio controls autoPlay loop key={videoSource}>
           <source src={videoSource} type="audio/mpeg" />
         </audio>
-        <p className="mt-5 mb-5 text-2xl" dangerouslySetInnerHTML={{ __html: sentence }} ></p>
+        <p className="mt-5 mb-5 text-2xl">{parse(sentence)}</p>
         <div className="flex flex-row pt-5 pb-5 space-x-10">
         <p className="p-3 border border-red-500 rounded-full ra-break">Pause</p>
         <p className="p-3 border border-orange-500 rounded-full ra-loss">Loss</p>
