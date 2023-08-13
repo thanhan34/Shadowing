f6bf67c67f3a3fc314a0e9d6971725b41b416af6
An Doan <dtan42@gmail.com>
2023-08-13 12:00:28 +1000

    key in paragraph

--- shadowing/pages/paragraph.tsx
+++ shadowing/pages/paragraph.tsx
@@ -1,48 +1,3 @@
-// import React, { useEffect } from "react";
-// import { addDoc, collection } from "firebase/firestore";
-// import { db } from "../firebase";
-
-// const Paragraph = () => {
-//   const data = [
-//     { name: "Name1", url: "URL1", text: "Text1" },
-//     { name: "Name2", url: "URL2", text: "Text2" },
-//     // ... add more objects as needed
-//   ];
-
-//   const handleSubmit = async () => {
-//     try {
-//       for (const item of data) {
-//         const { name, url, text } = item;
-//         if (name && url && text) {
-//           await addDoc(collection(db, "shadowing"), {
-//             name: name,
-//             url: url,
-//             text: text,
-//           });
-//           console.log("Document written to Firebase");
-//         } else {
-//           console.log("Incomplete data, skipping...");
-//         }
-//       }
-//     } catch (e) {
-//       console.error("Error adding to Firebase", e);
-//     }
-//   };
-
-//   useEffect(() => {
-//     // Automatically trigger handleSubmit when the component mounts
-//     handleSubmit();
-//   }, []);
-
-//   return (
-//     <main className="flex flex-col items-center justify-between min-h-screen p-24">
-//       {/* Your JSX content if needed */}
-//       <h1>Hello</h1>
-//     </main>
-//   );
-// };
-
-// export default Paragraph;
 import React, { useEffect, useState } from "react";
 import { getDownloadURL, ref, listAll } from "firebase/storage";
 import { storage } from "../firebase"; // Make sure to import the storage instance correctly
@@ -75,8 +30,8 @@ const Paragraph = () => {
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <h1>Download Links</h1>
       <ul>
-        {downloadLinks.map((link) => (
-          <p>{link.name},{link.url}</p>
+        {downloadLinks.map((link,index) => (
+          <p key={index}>{link.name},{link.url}</p>
           
         ))}
       </ul>
