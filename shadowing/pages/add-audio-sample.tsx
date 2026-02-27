02a5e704ea5e21026aac3a7cab182b1108c75d24
thanhan34 <dtan42@gmail.com>
2026-02-27 18:52:22 +0700

    feat: create alias page for /add-audio-sample with server-side redirect to /AddAudioSample

--- shadowing/pages/add-audio-sample.tsx
+++ shadowing/pages/add-audio-sample.tsx
@@ -1,1 +1,14 @@
-export { default } from './AddAudioSample'
+import type { GetServerSideProps } from 'next'
+
+const AddAudioSampleAliasPage = () => null
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: '/AddAudioSample',
+      permanent: false,
+    },
+  }
+}
+
+export default AddAudioSampleAliasPage
